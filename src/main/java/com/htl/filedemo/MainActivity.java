e57ccab689dc30dfbacd7f283c3e12af2b21ba5b
htl <1651825655@qq.com>
2017-03-17 17:36:33 +0800

    ''

--- src/main/java/com/htl/filedemo/MainActivity.java
+++ src/main/java/com/htl/filedemo/MainActivity.java
@@ -8,20 +8,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
 	private static final String TAG = "MainActivity";
 	private Object mExternalStorageDirectory;
 
-	private Button mBtnInput, mBtnOutput, mBtnRR;
+	private Button mBtnInput, mBtnOutput, mBtnRR, mBtnRw, mBtnSc, mBtnBuffer, mBtnSer;
 
 	private String content = "今天天气真好";
 	String path = Environment.getExternalStorageDirectory() + File.separator + "htl.text";
@@ -32,8 +39,13 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		initView();
-		listener();
-
+		streamrwFile();
+		inputFile();
+		outputFile();
+		rwFile();
+		scFile();
+		bufferFile();
+		serFile();
 		//是否挂载SDcard
 		boolean isMounted = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
 
@@ -47,9 +59,147 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	/**
-	 * 读写文件
+	 * 序列化
 	 */
-	private void listener() {
+	private void serFile() {
+
+		mBtnSer.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String wriPath = Environment.getExternalStorageDirectory() + File.separator + "htll.text";
+				Student student = new Student("小名", 21);
+				try {
+//					ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(wriPath));
+					ObjectOutputStream objectoutputStream = new ObjectOutputStream(new FileOutputStream(path));
+
+					objectoutputStream.writeObject(student);
+					objectoutputStream.close();
+
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+	/**
+	 * 缓冲流
+	 */
+	private void bufferFile() {
+		mBtnBuffer.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String wriPath = Environment.getExternalStorageDirectory() + File.separator + "htll.text";
+				try {
+					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(path))));
+					BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(wriPath))));
+
+					char[] chars = new char[10];
+					int len = 0;
+					while ((len = bufferedReader.read(chars)) != -1) {
+						bufferedWriter.write(chars, 0, len);
+						bufferedWriter.flush();
+					}
+					Toast.makeText(MainActivity.this, "读写成功", Toast.LENGTH_SHORT).show();
+					bufferedReader.close();
+					bufferedWriter.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+
+			}
+		});
+
+	}
+
+	/**
+	 * 字节流转为字符流
+	 */
+	private void scFile() {
+		mBtnSc.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				InputStreamReader inputStreamReader = null;
+				OutputStreamWriter outputStreamWriter = null;
+
+				try {
+					String wriPath = Environment.getExternalStorageDirectory() + File.separator + "htlll.text";
+					inputStreamReader = new InputStreamReader(new FileInputStream(new File(path)));
+					outputStreamWriter = new OutputStreamWriter(new FileOutputStream(new File(wriPath)));
+
+					char[] chars = new char[10];
+					int len = 0;
+					while ((len = inputStreamReader.read(chars)) != -1) {
+						outputStreamWriter.write(chars, 0, len);
+						outputStreamWriter.flush();
+					}
+					Toast.makeText(MainActivity.this, "读写成功", Toast.LENGTH_SHORT).show();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						inputStreamReader.close();
+						outputStreamWriter.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+	}
+
+	/**
+	 * 字符流读写文件
+	 */
+	private void rwFile() {
+		mBtnRw.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				FileReader fileReader = null;
+				FileWriter fileWriter = null;
+				try {
+//					String wriPath = Environment.getExternalStorageDirectory() + File.separator + "htl" + File.separator + "htll.text";
+					String wriPath = Environment.getExternalStorageDirectory() + File.separator + "htll.text";
+
+					fileReader = new FileReader(new File(path));
+					fileWriter = new FileWriter(new File(wriPath));
+
+					char[] chars = new char[4];
+					int len = 0;
+
+					if ((len = fileReader.read(chars)) != -1) {
+						fileWriter.write(chars, 0, len);
+						fileWriter.flush();
+					}
+					Toast.makeText(MainActivity.this, "读写成功", Toast.LENGTH_SHORT).show();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						fileReader.close();
+						fileWriter.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+
+	}
+
+	/**
+	 * 将文字写入到指定文件中
+	 */
+	private void outputFile() {
 		mBtnOutput.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -85,34 +235,39 @@ public class MainActivity extends AppCompatActivity {
 				}
 			}
 		});
+	}
 
+
+	/**
+	 * 从指定目录中读取文件
+	 */
+	private void inputFile() {
 		mBtnInput.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				FileInputStream inputStream = null;
-				File file = new File(path);
 				try {
-					inputStream = new FileInputStream(file);
+					File file = new File(path);
+					FileInputStream inputStream = new FileInputStream(file);
+
 					byte[] bytes = new byte[inputStream.available()];
 					inputStream.read(bytes);
 					String str = new String(bytes);
+
 					Log.d(TAG, "onClick: " + str);
+					inputStream.close();
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
 					e.printStackTrace();
-				} finally {
-					if (inputStream != null) {
-						try {
-							inputStream.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
 				}
 			}
 		});
+	}
 
+	/**
+	 * 字节流读写文件
+	 */
+	private void streamrwFile() {
 		mBtnRR.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -144,6 +299,10 @@ public class MainActivity extends AppCompatActivity {
 		mBtnInput = (Button) findViewById(R.id.btn_input);
 		mBtnOutput = (Button) findViewById(R.id.btn_output);
 		mBtnRR = (Button) findViewById(R.id.btn_rr);
+		mBtnRw = (Button) findViewById(R.id.btn_rw);
+		mBtnSc = (Button) findViewById(R.id.btn_sc);
+		mBtnBuffer = (Button) findViewById(R.id.btn_buffer);
+		mBtnSer = (Button) findViewById(R.id.btn_ser);
 	}
 
 	/**
---
+++ src/main/java/com/htl/filedemo/Student.java
@@ -0,0 +1,48 @@
+package com.htl.filedemo;
+
+import java.io.Serializable;
+
+/**
+ * 作者：htl
+ * 时间：2017/3/17:17:21
+ * 邮箱：
+ * 说明：
+ */
+class Student implements Serializable {
+
+
+	private String name;//学生姓名
+	private transient int age;//年龄设置为瞬时变量，将不被序列化
+
+
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	@Override
+	public String toString() {
+		return "该学生的名字为：" + name + "，年龄为：" + age;
+	}
+}
